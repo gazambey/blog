c96b3469a8aa7ac268ed183f3018ba3758385457
gazambey <gaston.ndoumbe@gmail.com>
2022-02-07 15:21:44 +0100

    Update New-Template.ps1

--- New-Template.ps1
+++ New-Template.ps1
@@ -1,1 +1,10 @@
-New-OMEConfiguration -Identity "GAZALABS" -EmailText " Ce courriel et ses pieces jointes sont confidentielles et sont destines exclusivement a l'usage de l'individu auquel il est adressé" -PortalText "GAZALABS INC" -DisclaimerText "GAZALABS INC. Tous droits reservés" -BackgrounColor "#385998" -OTPEnabled $true -SocialIdSignIn $True -Image (Get-Content "C:\Branding\logo.png" -Encoding byte) -ReadButtonText "lire un message securisé" -IntroductionText $null -ExternalMailExpiryInDays 10  
+New-OMEConfiguration -Identity "GAZALABS" 
+-EmailText " Ce courriel et ses pieces jointes sont confidentielles et sont destines exclusivement a l'usage de l'individu auquel il est adressé" 
+-PortalText "GAZALABS INC" -DisclaimerText "GAZALABS INC. Tous droits reservés" 
+-BackgrounColor "#385998" 
+-OTPEnabled $true 
+-SocialIdSignIn $True 
+-Image (Get-Content "C:\Branding\logo.png" -Encoding byte) 
+-ReadButtonText "lire un message securisé" 
+-IntroductionText $null 
+-ExternalMailExpiryInDays 10  
